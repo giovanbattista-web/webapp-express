15e8e54e5d323da0753e694ba5ad209adedbcd33
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-29 17:02:14 +0200

    [UPD] INDEX

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -1,7 +1,16 @@
 const connection = require('../data/db');
 
 const index = (req, res) => {
-    res.send("Elenco film");
+    const sql = "SELECT * FROM movies";
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: "Database query failed" });
+        }
+
+        // console.log(results);
+        res.json(results);
+
+    });
 };
 
 const show = (req, res) => {
