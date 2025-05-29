7e82a965be07271b2873d5cfb07b252e107e5e41
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-29 15:08:04 +0200

    [UPD] SERVER IN ASCOLTO E COLLEGAMENTO A POSTMAN

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
+
+
+
+app.get("/", (req, res) => {
+    console.log("Server dei miei film");
+    res.send("Benvenuto nel mio cinema")
+});
+
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`);
+})
